a184d2ea1cd46655373eabcb7838924d323871f9
Dale Bobermien <dalebobermien@outlook.com>
2022-03-09 20:14:32 +1000

    Add files via upload
    Initial commit to save my progress so far. Very much still a WIP as i build out the functionality

---
+++ What are the odds?/What are the odds?Tests/What_are_the_odds_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  What_are_the_odds_Tests.swift
+//  What are the odds?Tests
+//
+//  Created by Dale Bobermien on 9/3/2022.
+//
+
+import XCTest
+@testable import What_are_the_odds_
+
+class What_are_the_odds_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
