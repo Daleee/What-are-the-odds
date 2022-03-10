260a06a8dddb0d60f9046e376ed3be2077fadb41
Dale Bobermien <dalebobermien@outlook.com>
2022-03-10 11:40:42 +1000

    Removed Array from pickerView
    Optimised, so no longer need to hold an array

--- What are the odds?/ViewController.swift
+++ What are the odds?/ViewController.swift
@@ -12,13 +12,13 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         return 1
     }
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return availableOdds.count
+        return 50
     }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return availableOdds[row]
+        return String(row + 1)
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        chosenOdds = availableOdds[row]
+        chosenOdds = String(row + 1)
     }
     //Connection Variables from Storyboard to ViewController
     
@@ -40,10 +40,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     
     var chosenOdds = ""
-    
-    //Array for pickerView (Need to optimise)
-    
-    let availableOdds = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50"]
     
     //Loading the view
     
